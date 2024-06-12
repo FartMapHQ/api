75066703e1a6aee20e8ddc331ee87e2c34e3e44a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-12 18:29:50 +0200

    feat: add swagger and jwt handlers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { Elysia } from "elysia";
-import connector from "./database/connector";
+import { swagger } from '@elysiajs/swagger'
+
+import database from "./database";
 import { Fart } from "./database/models/fart";
 
+import auth from "./routers/auth";
+
+if (!process.env.JWT_SECRET) {
+  console.error("JWT_SECRET is not defined !");
+  process.exit(1);
+}
+
 new Elysia()
+  .use(swagger())
+  .use(auth)
   .get("/", () => ({
     success: true,
     message: "FartMap API is running !"
   }))
+  
+
   .get("/farts", async (req) => {
-    await connector.ensureConnection();
+    await database.ensureConnection();
     const farts = await Fart.find();
     return farts;
   })
+
   .listen(3000);
