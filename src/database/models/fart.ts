b486120e5cd2a400246dfc7e6a6153693995a5cf
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-12 18:34:00 +0200

    chore(models/fart): remove white spaces

--- src/database/models/fart.ts
+++ src/database/models/fart.ts
@@ -1,10 +1,8 @@
 import * as mongoose from 'mongoose';
 
-const schema = new mongoose.Schema(
-  {
-    description: { type: String, required: true }
-  }
-);
+const schema = new mongoose.Schema({
+  description: { type: String, required: true }
+});
 
 export type Fart = mongoose.InferSchemaType<typeof schema>;
 export const Fart = mongoose.model('Fart', schema);
